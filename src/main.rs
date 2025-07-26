ae696180441dff02948cb1d29aafd6f07a28e8f6
Juan Ignacio Rios <juani.rios.99@gmail.com>
2025-07-26 20:06:14 -0300

    convert impl

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,11 @@ pub trait Convert {
     fn convert(self) -> Self::Output;
     fn convert_back(output: Self::Output) -> Self;
 }
+
+impl Convert for i32 {
+    type Output = f64;
+
+    fn convert(self) -> Self::Output {
+        self as f64
+    }
+}
